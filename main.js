43bb00c9e7809503c6356c8dbac90dfeec307959
girlygado <40450213+girlygado@users.noreply.github.com>
2018-07-03 17:06:41 +0800

    With accordions -- v0.1
    with category toggle buttons working

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 
+/*-- Controls categories minimizeand maximize buttons --*/
 
-let categoryToggleButtons = document.querySelectorAll('.expand-btn');
+let categoryToggleButtons = document.querySelectorAll('.cat-expand-btn');
 
 for ( let i = 0; i < categoryToggleButtons.length; i++) {
-  
-  
   categoryToggleButtons[i].addEventListener('click', () => {
-    categoryToggleButtons[i].classList.toggle('collapse-btn');
+    categoryToggleButtons[i].classList.toggle('cat-collapse-btn');
 
+    let categoryClassName = categoryToggleButtons[i].className;
     let foodCat = categoryToggleButtons[i].previousElementSibling.getAttribute('data-category');
     let foodMenu = document.querySelector('#'+foodCat+">.card-body");
-    foodMenu.style.display = 'none';
+
+    if (categoryToggleButtons[i].classList.contains('cat-collapse-btn') == true) {
+      foodMenu.style.display = 'none';
+      document.querySelector('#'+foodCat).style.borderRadius = "20px";
+      document.querySelector('#'+foodCat+">.card-header").style.borderRadius = "20px";
+      //document.querySelector('#'+foodCat+">.card-header>span").classList.toggle('cat-collapse-btn');
+    } else {
+      foodMenu.style.display = 'block';
+      document.querySelector('#'+foodCat+">.card-header").style.borderRadius = "20px 20px 0px 0px";
+    }
+    
+  });
+}
+
+/*-- Controls on main menu minimizeand maximize buttons --*/
+
+let menuCardHeader = document.querySelectorAll('.food-category>.card-header');
+
+for (let i = 0; i < menuCardHeader.length; i++) {
+  menuCardHeader[i].addEventListener('click', () => {
+    console.log(menuCardHeader[i].innerHTML);
   })
 }
